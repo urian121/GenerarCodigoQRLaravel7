d9a781523f00575cf92682ffa9bc19bde9d29f46
Urian <urian1213viera@gmail.com>
2021-04-05 15:33:49 -0500

    Iniciando proyecto

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\empleado;
+use Illuminate\Http\Request;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(empleado $empleado)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_05_202625_create_empleados_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('sexo');
+            $table->bigInteger('cedula');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
